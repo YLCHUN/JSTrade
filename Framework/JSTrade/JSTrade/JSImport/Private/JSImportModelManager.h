949766330f169bf2d9c48be80927b8a427dfc766
YLCHUN <>
2019-04-24 16:08:01 +0800

    no message

--- Framework/JSTrade/JSTrade/JSImport/Private/JSImportModelManager.h
+++
@@ -1,9 +0,0 @@
-//
-//  JSImportModelManager.h
-//  JSTrade
-//
-//  Created by YLCHUN on 2017/7/20.
-//  Copyright © 2017年 ylchun. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
--- README.md
+++
@@ -1,4 +0,0 @@
-# WKWebView_JSTrade
-WKWebView JSTrade：JSExport、JSImport
-
-![说明](https://github.com/youlianchun/WKWebView_JSTrade/blob/master/%E8%AF%B4%E6%98%8E.png)
